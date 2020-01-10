9a3366e8a47745a3b75927a083b3dd2f0c51bd01
ttokunag <ttokunag@ucsd.edu>
2020-01-10 14:43:44 +0000

    implement BSTNode

--- src/bst/BSTNode.hpp
+++ src/bst/BSTNode.hpp
@@ -21,7 +21,11 @@ class BSTNode {
     BSTNode<Data>* parent;
 
     /** TODO */
-    BSTNode(const Data& d) : data(d) {}
+    BSTNode(const Data& d) : data(d) {
+        left = nullptr;
+        right = nullptr;
+        parent = nullptr;
+    }
 
     /** Set the value of data */
     void setData(const Data& d) { data = d; }
